db52e3bfaf11296f4e680c522dec21ba3bbf614e
huanglei1 <huanglei1@joyy.com>
2024-06-29 23:33:10 +0800

    feat:修改bert_test3.py的内容格式

--- bert_test3.py
+++ bert_test3.py
@@ -1,7 +1,42 @@
+"""
+File: bert_test3.py
+Author: xieyuyan
+Date: 2024-06-29
+
+Description:
+集成test和train
+通过RUN_TYPE调整训练和test
+    RUN_TYPE = train
+    RUN_TYPE = test
+Inputs:
+- bert-base-uncased
+
+Outputs:
+outputs bert的结构化输出
+
+"""
 # 假设你已经下载了WN18RR数据集并解压缩到适当的路径下
 # bert模型测试
 # 参考: https://zhuanlan.zhihu.com/p/605020970
 import os
+# 可选：进行数据预处理、处理成模型需要的输入格式
+# 例如：将实体和关系映射成BERT的输入ID，或者使用预训练的BERT模型来获得实体和关系的表示
+import torch
+import torch.nn as nn
+from transformers import BertModel
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+from transformers import BertTokenizer
+
+RUN_TYPE = 'test'
+
+# 设置一些超参数
+MAX_SEQ_LENGTH = 128
+BATCH_SIZE = 32
+LEARNING_RATE = 2e-5
+NUM_EPOCHS = 5
+root_path = os.path.dirname(__file__)
+
 # 读取数据集
 def load_data(filename):
     with open(filename, 'r') as f:
@@ -9,20 +44,9 @@ def load_data(filename):
     data = [line.strip().split('\t') for line in lines]
     return data
 
-root_path = os.path.dirname(__file__)
-path=os.path.join(root_path, 'data', 'WN18RR')
-train_data = load_data(path+'/train.txt')
-valid_data = load_data(path+'/valid.txt')
-test_data = load_data(path+'/test.txt')
-
-# 可选：进行数据预处理、处理成模型需要的输入格式
-# 例如：将实体和关系映射成BERT的输入ID，或者使用预训练的BERT模型来获得实体和关系的表示
-import torch
-import torch.nn as nn
-from transformers import BertModel
-
 
 class BERT_KG(nn.Module):
+
     def __init__(self, num_relations, bert_model_name=os.path.join(root_path, 'checkpoints','bert-base-uncased')):
         super(BERT_KG, self).__init__()
         self.bert = BertModel.from_pretrained(bert_model_name)
@@ -36,23 +60,6 @@ class BERT_KG(nn.Module):
         return logits
 
 
-import torch.optim as optim
-from torch.utils.data import DataLoader, TensorDataset
-from transformers import BertTokenizer
-
-# 设置一些超参数
-MAX_SEQ_LENGTH = 128
-BATCH_SIZE = 32
-LEARNING_RATE = 2e-5
-NUM_EPOCHS = 5
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-# 初始化模型和优化器
-model = BERT_KG(num_relations=11).to(device)
-optimizer = optim.AdamW(model.parameters(), lr=LEARNING_RATE)
-
-tokenizer = BertTokenizer.from_pretrained(os.path.join(root_path, 'checkpoints','bert-base-uncased'))
 
 def string_to_ascii_tensor(s):
     ascii_values = [ord(c) for c in s]  # 将每个字符转换为ASCII值
@@ -60,7 +67,7 @@ def string_to_ascii_tensor(s):
     return tensor
 
 # 准备数据
-def prepare_data(data):
+def prepare_data(data,tokenizer):
     input_ids = []
     attention_masks = []
     labels = []
@@ -95,16 +102,12 @@ def prepare_data(data):
     return TensorDataset(input_ids, attention_masks, labels)
 
 
-train_dataset = prepare_data(train_data)
-train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-
-# 定义损失函数和训练过程
-criterion = nn.CrossEntropyLoss()
-
-predictions = []
-true_labels = []
 
 def train(model, optimizer, train_loader, num_epochs):
+
+    predictions = []
+    true_labels = []
+
     model.train()
     epoch=0
     # for epoch in range(num_epochs):
@@ -134,9 +137,6 @@ def train(model, optimizer, train_loader, num_epochs):
 # train(model, optimizer, train_loader, NUM_EPOCHS)
 
 
-test_dataset = prepare_data(test_data)
-test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
-
 def test(model, optimizer, test_loader, num_epochs):
     model.load_state_dict(torch.load('bert.model'))
     model.eval()
@@ -150,7 +150,36 @@ def test(model, optimizer, test_loader, num_epochs):
             print(outputs)
     pass
 
-test(model,optimizer,test_loader,num_epochs=NUM_EPOCHS)
+
+if __name__ == '__main__':
+
+    path = os.path.join(root_path, 'data', 'WN18RR')
+    train_data = load_data(path + '/train.txt')
+    valid_data = load_data(path + '/valid.txt')
+    test_data = load_data(path + '/test.txt')
+
+    tokenizer = BertTokenizer.from_pretrained(os.path.join(root_path, 'checkpoints', 'bert-base-uncased'))
+
+    train_dataset = prepare_data(train_data,tokenizer)
+
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # 初始化模型和优化器
+    model = BERT_KG(num_relations=11).to(device)
+    optimizer = optim.AdamW(model.parameters(), lr=LEARNING_RATE)
+
+    # 定义损失函数和训练过程
+    criterion = nn.CrossEntropyLoss()
+
+    if RUN_TYPE == 'train':
+        train(model, optimizer, train_loader, NUM_EPOCHS)
+    else:
+        test_dataset = prepare_data(test_data,tokenizer)
+        test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True)
+        test(model, optimizer, test_loader, num_epochs=NUM_EPOCHS)
+
 # # 计算评估指标（这里假设是分类任务，可以根据具体任务调整评估方法）
 # from sklearn.metrics import accuracy_score, classification_report
 #
